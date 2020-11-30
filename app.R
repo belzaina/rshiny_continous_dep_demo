c540c088accb8ab85b2a7aa6b214ab08b1ad705d
Zainab BELGADA <belgadazainab@gmail.com>
2020-11-30 12:53:01 +0100

    first commit

---
+++ app.R
@@ -0,0 +1,31 @@
+#' Continuous Deployment Demo
+#' 
+#' Template Project for github - travis-ci - shinyapps.io workflow
+#' 
+#' @author Zainab BELGADA
+#'
+
+library(shiny)
+
+
+date_time <- date()
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    h1("DEMO APP"),
+    img(src = "https://travis-ci.com/belzaina/postefrance.svg?branch=master"),
+    br(),br(),
+    h3("Last build:"),
+    p(date_time)
+    
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
